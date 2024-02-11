37c82bcad796ec099b5d4166b8620c762d511927
MIRZA AHMER BAIG <ahmermirza857@gmail.com>
2024-02-11 23:54:30 +0500

    PYTHON_LEARNING

---
+++ chapter_1.py
@@ -0,0 +1,35 @@
+print("HELLO WORLD")
+print("HELLO WORLD","MY NAME IS MIRZA AHMER BAIG")
+#     VARAIBLE FOR PYTHON
+name = "MIRZA AHMER BAIG"
+age = None
+print(age)
+
+print(name)
+age = 45
+print(age)
+name= "usman"
+print(name)
+var = True
+print(type(var))
+ 
+#   there are five data types in 5 python
+#  int float string boolean nonetype
+var= None
+print(type(var))
+
+# python is a case sensitive language
+
+a=45
+b=45
+print( a==b)
+print(6**2)
+a=7
+a += a
+print(a)
+
+# LOGICAL OPERATOR
+#  AND OR IT WOWK ON TWO VALUE
+# NOT WILL WORK ON ONE VALUE
+
+
