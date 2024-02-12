924eb1663f81597444b70c589065084e11cd3375
MIRZA AHMER BAIG <ahmermirza857@gmail.com>
2024-02-13 01:11:17 +0500

    chapter 2complete

--- chapter_2.py
+++ chapter_2.py
@@ -1,4 +1,5 @@
 #      chapter 2   Strings & Conditional Statements 
+
 str1= "this  is  method 1 for creating string"
 str2= 'this method 2 for creating string'
 str3= """"this is third method of creating string"""
@@ -25,6 +26,49 @@ print(len(num1))
 string1="my name is mirza ahmer baig"
 print(string1[4])
 # string1[0]="d" this is not possible onle access data not changeed in string
+
+#    slicing
+
+name1="my name is mirza ahmer baig"
+print(name1[1:])
+print(name1[1:5])
+print(name1[3:7])
+
+#  backword counting is also worked
+
+print(name1.replace("my","your"))
+
+print(name1)
+
+name1=name1.replace("my","your")
+
+print(name1)
+#  PRACTICE QUESTION
+
+firstname=input("ENTER YOUR FIRST NAME: ")
+fl=len(firstname)
+print("THE LENGTH OF YOUR FIRSTNAME IS : " , fl)
+
+#         CONDITIONAL STATMENT
+# if elif else
+
+marks=int(input("ENTER YOUR MARKS OUT OF 100: "))
+if(marks>=90):
+    print("YOU GOT A GRADE")
+elif(marks>=80):
+    print("YOU GOT B GRADE")
+elif(marks>=70):
+    print("YOU GOT B GRADE")
+else:
+    print("fail")
+
+
+    
+
+
+
+
+
 
 
 
