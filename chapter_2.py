d2df7d08a216dbdb7a5788190f06769958839031
MIRZA AHMER BAIG <ahmermirza857@gmail.com>
2024-02-12 23:51:40 +0500

    chapter_2

---
+++ chapter_2.py
@@ -0,0 +1,32 @@
+#      chapter 2   Strings & Conditional Statements 
+str1= "this  is  method 1 for creating string"
+str2= 'this method 2 for creating string'
+str3= """"this is third method of creating string"""
+
+#  escape sequence character
+
+#  for breaking line /n
+# for tab /t
+#  for concatination +
+
+# len() is used for finding length of string
+
+print(len(str1))
+print(len(str2))
+print(len(str3))
+
+num1=str(123455667788990)
+print(type(num1))
+print(len(num1))
+
+#  indexing
+
+#  indexing is start at zero
+string1="my name is mirza ahmer baig"
+print(string1[4])
+# string1[0]="d" this is not possible onle access data not changeed in string
+
+
+
+
+
